5caf41db980dde60db1aa4d5952ea0f7fc442cd1
marazmatikpepe-coder <marazmatikpepe@gmail.com>
2026-06-10 20:17:32 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAd48m-Xg1E1Y7EY73xLkVPqs4uhGm9-hg",
+    authDomain: "k-os-36d06.firebaseapp.com",
+    projectId: "k-os-36d06",
+    storageBucket: "k-os-36d06.firebasestorage.app",
+    messagingSenderId: "780326437033",
+    appId: "1:780326437033:web:cc35923088f46bbe7729b8",
+    measurementId: "G-8DYMH0JSSS"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
